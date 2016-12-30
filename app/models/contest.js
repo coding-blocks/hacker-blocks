dd4d44ea2654252b0bbfdfb105cf74f683cf000d
Umair Khan <omerjerk@gmail.com>
2016-12-30 18:48:44 +0530

    perfect hooking with the data
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/models/contest.js
+++ app/models/contest.js
@@ -1,9 +1,13 @@
+/**
+ * Created by umair on 30/12/16.
+ */
+
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  id: DS.attr(),
   name: DS.attr(),
   date: DS.attr(),
   location: DS.attr(),
   image: DS.attr()
 });
+
--- mirage/config.js
+++ mirage/config.js
@@ -1,20 +1,28 @@
 export default function() {
+
   this.namespace = '/api';
 
-  this.get('/contests', function () {
+  this.get('/contests', function() {
     return {
       data: [{
         id: 1,
-        name: "The Great Coding Blocks Hackathon",
-        date: "12th Dec 9:00 am",
-        location: "Pitampura Center",
-        image: "http://www.gravatar.com/avatar/0cf15665a9146ba852bf042b0652780a?s=200"
+        type: "contest",
+        attributes: {
+          name: "The Great Coding Blocks Hackathon",
+          date: "12th Dec 9:00 am",
+          location: "Pitampura Center",
+          image: "http://www.gravatar.com/avatar/0cf15665a9146ba852bf042b0652780a?s=200"
+        }
+
       },{
         id: 2,
-        name: "Competitive Hackathon",
-        date: "12th Dec 9:00 am",
-        location: "Online",
-        image: "http://www.gravatar.com/avatar/0cf15665a9146ba852bf042b0652780a?s=200"
+        type: "contest",
+        attributes: {
+          name: "Competitive Hackathon",
+          date: "12th Dec 9:00 am",
+          location: "Online",
+          image: "http://www.gravatar.com/avatar/0cf15665a9146ba852bf042b0652780a?s=200"
+        }
       }]
     };
   });
