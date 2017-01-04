b6711d2efd1c5df0432386bea68147f966c0afe1
Umair Khan <omerjerk@gmail.com>
2017-01-04 12:28:58 +0530

    fethc data using API in problem page
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- mirage/config.js
+++ mirage/config.js
@@ -1,5 +1,7 @@
 export default function() {
 
+  let lorem_ipseum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
   this.namespace = '/api';
 
   this.get('/contests', function() {
@@ -55,7 +57,7 @@ export default function() {
         type: "problem",
         attributes: {
           name: "The Great Coding Blocks Conundrum",
-          content: "Lorem ipsum..."
+          content: lorem_ipseum
         }
       }
     };
