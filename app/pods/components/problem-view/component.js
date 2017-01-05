4a864a954c13f5b2d72d71dc8cb21790622475ef
Umair Khan <omerjerk@gmail.com>
2017-01-05 14:26:04 +0530

    add comment for tabs hack
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/components/problem-view/component.js
+++ app/pods/components/problem-view/component.js
@@ -5,6 +5,7 @@ var tabInitDone = false;
 export default Ember.Component.extend({
   didRender() {
     this._super(...arguments);
+    //We don't want to init tabs again and again else tabs will break
     if (!tabInitDone) {
       $('ul.tabs').tabs();
       tabInitDone = true;
