1947164424c2abca1398b124993e8c5fb59e6260
Umair Khan <omerjerk@gmail.com>
2017-01-06 16:45:12 +0530

    Connect contests page to real API
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/application/adapter.js
+++ app/pods/application/adapter.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-  namespace: 'api'
+  namespace: 'api',
+  host: 'http://localhost:3000'
 });
--- mirage/config.js
+++ mirage/config.js
@@ -2,8 +2,8 @@ export default function() {
 
   let lorem_ipseum = "Lorem ipsum dolor sit `amet`, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-  this.namespace = '/api';
-
+  this.namespace = 'http://localhost:3000/api';
+/*
   this.get('/contests', function() {
     return {
       data: [{
@@ -27,7 +27,8 @@ export default function() {
         }
       }]
     };
-  });
+  });*/
+  this.passthrough('/contests');
 
   this.get('/contests/:id', function () {
     return {
