4b3f3c3dcde2d3877556621a8432040176ab04f0
Umair Khan <omerjerk@gmail.com>
2017-01-09 15:46:34 +0530

    derp
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/contests/contest/problem/leaderboard/route.js
+++ app/pods/contests/contest/problem/leaderboard/route.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   ajax: Ember.inject.service(),
   model() {
-    return this.get('store').query('submission', {problem_id : 1, leaderboard: true});
-    // return this.get('ajax').request('http://localhost:3000/api/submisions?problem_id=' + 1);
+    var problem = this.modelFor('contests.contest.problem');
+    return this.get('store').query('submission', {problem_id : problem.id, leaderboard: true});
   }
 });
