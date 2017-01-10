d56dcd9f842d40240f3efe4533b039b8d2cf1fd7
Umair Khan <omerjerk@gmail.com>
2017-01-10 11:25:01 +0530

    change editor mode along with language
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/contests/contest/problem/controller.js
+++ app/pods/contests/contest/problem/controller.js
@@ -1,15 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  lang : {
-    "c": "C",
-    "cpp": "C++",
-    "py2": "Python",
-    "java": "Java"
-  },
-  actions: {
-    langChange(langId) {
-      $("#editor-lang").text(this.lang[langId]);
-    }
-  }
 });
--- app/pods/contests/contest/problem/main/controller.js
+++ app/pods/contests/contest/problem/main/controller.js
@@ -1,8 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  init: function () {
-    this._super();
-
+  lang : {
+    "c": {
+      name: "C",
+      mode: "ace/mode/c_cpp"
+    },
+    "cpp": {
+      name: "C++",
+      mode: "ace/mode/c_cpp"
+    },
+    "py2": {
+      name: "Python",
+      mode: "ace/mode/python"
+    },
+    "java": {
+      name: "Java",
+      mode: "ace/mode/java"
+    }
   },
+  actions: {
+    langChange(langId) {
+      $("#editor-lang").text(this.lang[langId]["name"]);
+      ace.edit("editor").getSession().setMode(this.lang[langId]["mode"])
+    }
+  }
 });
