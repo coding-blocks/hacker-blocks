0e1f0d7c8e839d76d356ff38c0b44859ee4137bc
Umair Khan <omerjerk@gmail.com>
2017-01-15 16:50:05 +0530

    add more parameters to problems
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/models/problem.js
+++ app/models/problem.js
@@ -6,5 +6,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr(),
-  content: DS.attr()
+  content: DS.attr(),
+  submitCount: DS.attr(),
+  difficulty: DS.attr()
 });
