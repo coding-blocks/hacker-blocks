0df91c5a68b555ab8d1493c811ef8e503a71dfcf
Umair Khan <omerjerk@gmail.com>
2017-01-21 14:09:01 +0530

    move leaderboard to a separate component
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/practice/weekly-problem/leaderboard/route.js
+++ app/pods/practice/weekly-problem/leaderboard/route.js
@@ -6,7 +6,7 @@ export default Ember.Route.extend({
   },
   ajax: Ember.inject.service(),
   model() {
-    var id = this.modelFor('contests.contest.problem').problem_id;
+    var id = this.modelFor('practice.weekly-problem').problem_id;
     return this.get('store').query('submission', {problem_id : id, leaderboard: true});
   }
 });
