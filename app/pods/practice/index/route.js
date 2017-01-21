4fd5a9533056b7c3a369018dac1ffda3a43d4117
Umair Khan <omerjerk@gmail.com>
2017-01-21 11:26:36 +0530

    weekly problems beta
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/practice/index/route.js
+++ app/pods/practice/index/route.js
@@ -3,5 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   breadCrumb: {
     title: "Practice"
+  },
+  model() {
+    return this.get('store').query('problem', {weekly: true});
   }
 });
