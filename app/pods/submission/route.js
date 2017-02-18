6ff243009ca88ffb19af900fe46951b37c379e13
Umair Khan <omerjerk@gmail.com>
2017-02-18 13:47:59 +0530

    derp
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

---
+++ app/pods/submission/route.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  actions: {
+    didTransition() {
+      // Ember.run.scheduleOnce('afterRender', this, function () {
+      //
+      // });
+    }
+  }
+});
