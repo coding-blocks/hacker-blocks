49ee6f7251d170859241335fc75a086264c4ee22
aayusharora <aayush113002@gmail.com>
2017-03-08 02:29:45 +0530

    Getting rid JS animations

--- app/pods/components/nav-bar/component.js
+++ app/pods/components/nav-bar/component.js
@@ -3,18 +3,6 @@ import initNavAnim from '../../../utils/init-nav-anim';
 
 export default Ember.Component.extend({
   didRender() {
-    var $el, leftPos, newWidth,
-      $mainNav = $("#main-menu");
-
-    $mainNav.append("<li id='magic-line'></li>");
-    var $magicLine = $("#magic-line");
-
-    $magicLine
-      .width($("#main-menu .active a").width())
-      .css("left", $("#main-menu .active a").offset().left)
-      .data("origLeft", $magicLine.position().left)
-      .data("origWidth", $magicLine.width());
-
        initNavAnim();
   }
 });
