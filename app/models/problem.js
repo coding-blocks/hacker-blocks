281061d1c9c03d6dc710cd579bc54daff7788b81
Umair Khan <omerjerk@gmail.com>
2017-03-08 13:28:48 +0530

    show day and month on daily codebytes
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/models/problem.js
+++ app/models/problem.js
@@ -5,6 +5,8 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
 export default DS.Model.extend({
   name: DS.attr(),
   content: DS.attr(),
@@ -13,5 +15,14 @@ export default DS.Model.extend({
   }),
   submitCount: DS.attr(),
   difficulty: DS.attr(),
-  image: DS.attr()
+  image: DS.attr(),
+  day: DS.attr(),
+  UTCDay: Ember.computed('day', function () {
+    console.log(this.get('day'));
+    return this.get('day').toString().substring(6,8);
+  }),
+  UTCMonth: Ember.computed('day', function () {
+    let month = this.get('day').toString().substring(4, 6);
+    return months[parseInt(month)];
+  })
 });
