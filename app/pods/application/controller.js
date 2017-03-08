4ac8eb11defcd42fbd9b22ca0d0ccc1c52cedb76
Umair Khan <omerjerk@gmail.com>
2017-03-08 13:35:48 +0530

    fix login/logout issue
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -1,13 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  session: Ember.inject.service('session'),
-  actions: {
-    openLoginDialog() {
-      $('#login-dialog').modal('open');
-    },
-    invalidateSession() {
-      this.get('session').invalidate();
-    }
-  }
 });
--- app/pods/components/nav-bar/component.js
+++ app/pods/components/nav-bar/component.js
@@ -4,5 +4,11 @@ import initNavAnim from '../../../utils/init-nav-anim';
 export default Ember.Component.extend({
   didRender() {
        initNavAnim();
+  },
+  session: Ember.inject.service('session'),
+  actions: {
+    invalidateSession() {
+      this.get('session').invalidate();
+    }
   }
 });
