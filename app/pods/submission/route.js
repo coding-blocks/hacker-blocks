ab4e393ca0e81d658eabcdea1500462ddf4170c6
Umair Khan <omerjerk@gmail.com>
2017-03-11 17:00:16 +0530

    fix submissions view
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/submission/route.js
+++ app/pods/submission/route.js
@@ -1,11 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  actions: {
-    didTransition() {
-      // Ember.run.scheduleOnce('afterRender', this, function () {
-      //
-      // });
-    }
+  breadCrumb: {
+    title: "Submission"
   }
 });
