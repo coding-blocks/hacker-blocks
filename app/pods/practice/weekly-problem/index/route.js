f1e0c3c515b4af8f5537acfabe4b86778926b9a9
Umair Khan <omerjerk@gmail.com>
2017-03-30 14:38:29 +0530

    fix submissions listing
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/practice/weekly-problem/index/route.js
+++ app/pods/practice/weekly-problem/index/route.js
@@ -8,14 +8,17 @@ var lang_codes = {
 };
 
 export default Ember.Route.extend({
+  session: Ember.inject.service('session'),
   breadCrumb: {
     title: 'Description'
   },
   model() {
-    var problem_id = this.modelFor('practice/weekly-problem').problem_id;
+    let userId = this.get('session.data.authenticated.user_id');
+    let problem_id = this.modelFor('practice/weekly-problem').problem_id;
     return Ember.RSVP.hash({
       lang_codes: lang_codes,
-      problem: this.get('store').queryRecord('problem', {problem_id, weekly:true})
+      problem: this.get('store').queryRecord('problem', {problem_id, weekly:true}),
+      submissions: this.get('store').query('submission', {problem_id : problem_id, user_id: userId})
     });
   }
 });
