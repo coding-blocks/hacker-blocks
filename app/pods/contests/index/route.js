68dce8ae27f6eddad24a4ed5e797cb49c0cd901a
Gopal kumar <gopal.kumar@nagarro.com>
2017-03-30 09:47:48 +0000

    Contest-view Updated

--- app/pods/contests/index/route.js
+++ app/pods/contests/index/route.js
@@ -4,7 +4,39 @@ export default Ember.Route.extend({
   breadCrumb: {
     title: 'All Contests'
   },
-  model() {
-    return this.get('store').findAll('contest');
+
+  model: function () {
+    var activeContests = [];
+    var upcomingContests = [];
+    var previousContests = [];
+
+    this.get('store').findAll('contest').then(function (contest) {
+      var presentDate = Math.floor(new Date().valueOf() / 1000);
+      console.log(presentDate);
+      contest.forEach(function (element) {
+        if (element.get('startTime') <= presentDate && element.get('endTime') >= presentDate) {
+          activeContests.pushObject(element);
+        }
+        else if (element.get('startTime') > presentDate) {
+          upcomingContests.pushObject(element);
+        }
+        else {
+          previousContests.pushObject(element);
+        }
+      })
+    })
+    
+    return Ember.RSVP.hash({
+      activeContests: activeContests,
+      upcomingContests: upcomingContests,
+      previousContests: previousContests
+    });
+  },
+
+  setupController(controller, model) {
+    this._super(...arguments);
+    Ember.set(controller, 'activeContests', model.activeContests);
+    Ember.set(controller, 'upcomingContests', model.upcomingContests);
+    Ember.set(controller, 'previousContests', model.previousContests);
   }
 });
