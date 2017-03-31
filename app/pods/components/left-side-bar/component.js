bf645d23cd265d756e83841f5c91fdfeb7a41310
Umair Khan <omerjerk@gmail.com>
2017-03-31 17:31:28 +0530

    fix leftsidebar hamburger
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/components/left-side-bar/component.js
+++ app/pods/components/left-side-bar/component.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didRenderDone: false,
+  didRender() {
+    if (this.get('didRenderDone') === false) {
+      $.AdminBSB.leftSideBar.activate();
+      $.AdminBSB.navbar.activate();
+      this.set('didRenderDone', true);
+    }
+  },
   session: Ember.inject.service('session'),
   actions: {
     popup() {
