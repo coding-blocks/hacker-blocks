9fdc573cc0f175dca55076225bcf4ed1401b8c96
Umair Khan <omerjerk@gmail.com>
2017-04-05 12:05:59 +0530

    fix opening problem without logged in
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/practice/weekly-problem/index/route.js
+++ app/pods/practice/weekly-problem/index/route.js
@@ -13,12 +13,10 @@ export default Ember.Route.extend({
     title: 'Description'
   },
   model() {
-    let userId = this.get('session.data.authenticated.user_id');
     let problem_id = this.modelFor('practice/weekly-problem').problem_id;
     return Ember.RSVP.hash({
       lang_codes: lang_codes,
-      problem: this.get('store').queryRecord('problem', {problem_id, weekly:true}),
-      submissions: this.get('store').query('submission', {problem_id : problem_id, user_id: userId})
+      problem: this.get('store').queryRecord('problem', {problem_id, weekly:true})
     });
   }
 });
