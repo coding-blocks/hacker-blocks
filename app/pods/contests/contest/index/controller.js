6be55d677e431fd0614fb4be13d2edff003d98e2
Ankit Luthra <ankit.luthra@nagarro.com>
2017-04-06 13:59:33 +0530

    using setup controller and ajax request for submission count

--- app/pods/contests/contest/index/controller.js
+++ app/pods/contests/contest/index/controller.js
@@ -1,14 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    submissionCount:Ember.computed('model.contest', function() {
-     const problems = this.get('model.contest.problems');
-     console.log(problems.get('length'));
-     let count= 0;
-     problems.forEach(el=>{
-         count += parseInt( el.get('submitCount') );
-     });
-
-     return count;
-  })
 });
--- app/pods/contests/contest/index/route.js
+++ app/pods/contests/contest/index/route.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-
+import Env from '../../../../config/environment';
 export default Ember.Route.extend({
   breadCrumb: {
     title: 'Contest'
@@ -8,10 +8,33 @@ export default Ember.Route.extend({
     let contest = this.modelFor('contests.contest');
     return Ember.RSVP.hash({
       contest: contest,
-      leaderboard: this.get('store').query('submission', { contest_id: contest.id, leaderboard: true, contest: true })
+      leaderboard: this.get('store').query('submission', { contest_id: contest.id, leaderboard: true, contest: true }),
+
     });
   },
 
   afterModel(model) {
+  },
+  setupController: function (controller, model) {
+
+    this._super(controller, model);
+    let contest = model.contest;
+    let contestId = contest.get('id');
+    $.ajax({
+      url: Env.apiEndpoint + '/api/submissions/submissionCount',
+      type: 'GET',
+      data: { contestId: contestId },
+      accepts: 'application/json',
+      success: function (data) {
+        controller.set('submissionCount', data[0].count);
+      },
+      error: function () {
+         controller.set('submissionCount', 0);
+      }
+    });
+
+
+
   }
+
 });
