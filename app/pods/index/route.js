9c1cd188044a518291776694302eea9b592ec18a
Umair Khan <omerjerk@gmail.com>
2017-04-13 11:13:31 +0530

    fix dailyCB links on homepage
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
-      daily: this.get('store').query('problem', { weekly: true, per_page: 4 }),
+      dailyCBs: this.get('store').query('dailycb', {count: 4}),
       contests: this.get('store').query('contest', { public: true })
     });
   }
