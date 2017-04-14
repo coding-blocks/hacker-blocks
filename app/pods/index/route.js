95e9672ddad74f1552ededd6ca9bcbf564b88aab
Umair Khan <omerjerk@gmail.com>
2017-04-14 16:27:46 +0530

    show correct problems in home daily code bytes
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
+    let day = moment(moment().unix(), 'X').format("YYYYMMDD");
     return Ember.RSVP.hash({
-      dailyCBs: this.get('store').query('dailycb', {count: 4}),
+      dailyCBs: this.get('store').query('dailycb', {count: 4, day}),
       contests: this.get('store').query('contest', { public: true })
     });
   }
