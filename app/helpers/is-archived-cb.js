cac4e944808285198ef49dae38d20ab045b510f3
Umair Khan <omerjerk@gmail.com>
2017-04-19 14:02:36 +0530

    show warning that no points will be given in older codebytes
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

---
+++ app/helpers/is-archived-cb.js
@@ -0,0 +1,19 @@
+/**
+ * Created by umair on 4/19/17.
+ */
+
+import Ember from 'ember';
+import moment from 'npm:moment';
+
+export function isArchivedCB(dailyCB) {
+  dailyCB = dailyCB[0]; //No idea why is this an Array
+  let dayStamp = moment.unix(moment().unix()).format('YYYYMMDD');
+  if (dailyCB.get('day') < dayStamp) {
+    return true;
+  }
+  return false;
+}
+
+
+
+export default Ember.Helper.helper(isArchivedCB);
