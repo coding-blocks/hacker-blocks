ffdeb8dbd13ce514aef1e59f3d3ff96e86c1ff7a
Umair Khan <omerjerk@gmail.com>
2017-04-19 17:07:20 +0530

    fix: undefined error
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/helpers/is-archived-cb.js
+++ app/helpers/is-archived-cb.js
@@ -7,6 +7,9 @@ import moment from 'npm:moment';
 
 export function isArchivedCB(dailyCB) {
   dailyCB = dailyCB[0]; //No idea why is this an Array
+  if (dailyCB == undefined) {
+    return;
+  }
   let dayStamp = moment.unix(moment().unix()).format('YYYYMMDD');
   if (dailyCB.get('day') < dayStamp) {
     return true;
