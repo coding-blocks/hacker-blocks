9c79dd974bb06cc274d4610eab1ed8e5b6977d6b
Umair Khan <omerjerk@gmail.com>
2017-04-28 13:15:16 +0530

    fix: log out users who are using old system
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/authenticators/custom.js
+++ app/authenticators/custom.js
@@ -9,23 +9,18 @@ import env from '../config/environment';
 export default Base.extend({
   restore(data) {
     return new Ember.RSVP.Promise(function (resolve, reject) {
-      resolve(data);
+      if (data.auth_token) {
+        resolve(data);
+      } else {
+        console.log("Old logging system detected. Logging out.");
+        reject();
+      }
     });
   },
   authenticate() {
     var args = [...arguments];
     return new Ember.RSVP.Promise(function(resolve, reject) {
-      /*
-      let form = {};
-      if (args[0].indexOf('@') > -1) {
-        //user entered an email
-        form.username = args[0];
-      } else {
-        form.roll_number = args[0];
-      }
-      form.password = args[1];*/
       $.get(env.apiEndpoint + '/oneauth/login?grant_code=' + args[0], function (data) {
-        console.log(data);
         if (data.auth_token != undefined) {
           resolve(data);
         } else {
--- app/authorizers/custom.js
+++ app/authorizers/custom.js
@@ -6,7 +6,8 @@ import Base from 'ember-simple-auth/authorizers/base';
 
 export default Base.extend({
   authorize(sessionData, block) {
-    block("oauth-id", sessionData.oauth_id);
+    block("oauth-id", sessionData.oauth_id); //This won't be required most probably. Need to remove it.
     block("access-token", sessionData.auth_token);
+    block("user-id", sessionData.user_id);
   }
 });
