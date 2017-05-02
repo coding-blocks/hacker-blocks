0b723c94e5ae607d0ca9d762b0b8109c18760260
Umair Khan <omerjerk@gmail.com>
2017-05-02 15:24:59 +0530

    fix: various timed contest fixes
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/contests/contest/attempt/controller.js
+++ app/pods/contests/contest/attempt/controller.js
@@ -6,19 +6,27 @@ const { inject: { service }, Component } = Ember;
 export default Ember.Controller.extend({
   session:     service('session'),
   currentUser: service('current-user'),
+  checkAttempts() {
+    return new Promise((resolve, reject) => {
 
+    });
+  },
+  init() {
+    console.log("controller init()");
+  },
   actions: {
     attemptContest (contestId) {
       const userId = this.get('session').get('data').authenticated.user_id;
-
       Ember.$.post(
         `${ENV.apiEndpoint}/api/contest_attempts/${contestId}`,
-        { userId: userId },
-        (result) => {
-          this.transitionToRoute('contests.contest', contestId)
+          { userId: userId }, (result) => {
+          if (result.data != null) {
+            this.transitionToRoute('contests.contest', contestId);
+          } else {
+            this.set('error', 'You can\'t attempt this contest. You may have reached your max possible attempts on this contest.');
+          }
         }
-      )
-        .fail(() => console.log ("You done goofed!"));
+      ).fail(() => console.log ("You done goofed!"));
     }
   }
 });
