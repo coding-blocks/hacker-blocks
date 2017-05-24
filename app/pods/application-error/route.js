bff40f94b469a2cecb54996fe3a73c08a52f376d
Prajjwal Singh <sin@prajjwal.com>
2017-05-24 12:47:46 +0530

    Fix: Error Handling

--- app/pods/application-error/route.js
+++ app/pods/application-error/route.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  setupController(controller, errors) {
+    let error = errors.errors.get('firstObject')
+
+    if (! error.message) {
+      if (error.status === "403") {
+        error.message = 'You are not authorized to view that page.'
+      }
+
+      else if (error.status === "404") {
+        error.message = 'The page you asked for doesn\'t exist!'
+      }
+
+      else if (error.status === "500") {
+        error.message = 'Something Went Wrong!'
+      }
+
+    }
+
+    this._super(...arguments)
+  }
 });
--- app/pods/application/adapter.js
+++ app/pods/application/adapter.js
@@ -1,4 +1,3 @@
-import Ember from 'ember';
 import DS from 'ember-data';
 import env from '../../config/environment';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
