736828d14608973dee13219f9497c318e42439ee
Gopal kumar <gopal.kumar@nagarro.com>
2017-05-25 15:30:37 +0530

    Made changes for refreshing submissions under practice section

--- app/pods/contests/contest/problem/index/controller.js
+++ app/pods/contests/contest/problem/index/controller.js
@@ -3,9 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     redirectToContest(model) {
-      //this.get('target.router').refresh()
-   this.transitionToRoute('contests.contest.contest-result', model.contest.id);  
-  }
-
+        //this.get('target.router').refresh()
+    this.transitionToRoute('contests.contest.contest-result', model.contest.id);  
+    }
   }
 });
--- app/pods/practice-section/problems/problem/index/controller.js
+++ app/pods/practice-section/problems/problem/index/controller.js
@@ -5,7 +5,15 @@ export default Ember.Controller.extend({
     redirectToContest(model) {
       //this.get('target.router').refresh()
    this.transitionToRoute('contests.contest.contest-result', model.contest.id);  
-  }
+  },
+  refreshModel(){
+      var model = this.get('model');
+      var problem_id = model.problem.id;
+      var contest_id = model.contest.id;
+      this.get('store').queryRecord('problem', {problem_id, contest_id: contest_id}).then(function(fetchedProblem){
+        this.set('model.problem', fetchedProblem);
+      });
+    }
 
   }
 });
