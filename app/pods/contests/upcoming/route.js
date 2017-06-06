fe27b88ae273c2dc4ece20980f45cbd27c7feda5
Umair Khan <omerjerk@gmail.com>
2017-06-06 14:04:09 +0530

    fix indent
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/contests/upcoming/route.js
+++ app/pods/contests/upcoming/route.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import moment from 'moment';
 export default Ember.Route.extend({
      model(params) {
-    let contest = this.get('store').findRecord('contest', params.contest_id);
+       let contest = this.get('store').findRecord('contest', params.contest_id);
     // contest.then(function(con){
     //    console.log(con.get('name'));
     // });
