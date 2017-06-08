aaf3d391377e63c85256b219fed0ad83739e79a3
abhishek97 <zapper.awake@gmail.com>
2017-06-08 17:07:08 +0530

    yourRank from meta

--- app/pods/contests/contest/index/route.js
+++ app/pods/contests/contest/index/route.js
@@ -39,6 +39,7 @@ export default Ember.Route.extend({
   setupController: function (controller, model) {
     this._super(controller, model);
     controller.set('submissionCount', model.submissionCount ? model.submissionCount[0].count : 0 );
+    controller.set('yourRank', model.leaderboard.get('meta') );
   },
   afterModel(model, transition) {
     const { currentAttempt, contest } = model;
