42d7726f5fa7d332584d8f7196836fce11598fd7
Siddharth <sd5869@gmail.com>
2017-07-10 15:27:08 +0530

    Added Submission Count.

--- app/pods/components/contest-banner-view/component.js
+++ app/pods/components/contest-banner-view/component.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
-
+import ENV from '../../../config/environment';
 export default Ember.Component.extend({
+	submissionCount: null,
+	init () {
+        console.log("Phuck Ember");
+		this._super(...arguments);
+		Ember.$.ajax({
+			method: "GET",
+			data:{
+				contestId: this.get('contest.id'),
+			},
+			url: (ENV.apiEndpoint + '/api/submissions/submissionCount')
+		}).done(res => {
+			// console.log(res);
+			this.set("submissionCount",res[0].count);
+		}).fail (err => {
+            console.log(err);
+        });
+	}
 });
