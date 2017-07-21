1fdba14781e30b5075e1ac90d49068a0a368d90f
Umair Khan <omerjerk@gmail.com>
2017-07-21 14:57:31 +0530

    fix leaderboard in practice
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/practice/problems/problem/index/route.js
+++ app/pods/practice/problems/problem/index/route.js
@@ -10,8 +10,7 @@ export default Ember.Route.extend({
     let problem_id = this.modelFor('practice.problems.problem').problem_id;
     return Ember.RSVP.hash({
       problem: this.get('store').queryRecord('problem', {problem_id, contest_id: contest.id}),
-      contest: contest,
-      leaderboard: this.get('store').query('submission', {contest_id: contest.id, problem_id : problem_id, leaderboard: true}),
+      contest: contest
     });
   }
 });
