68902914f1997c62605afb491bd2765aed6155b8
abhishek97 <zapper.awake@gmail.com>
2017-07-28 17:51:56 +0530

    add auth with jwt

--- app/authorizers/custom.js
+++ app/authorizers/custom.js
@@ -7,7 +7,7 @@ import Base from 'ember-simple-auth/authorizers/base';
 export default Base.extend({
   authorize(sessionData, block) {
     block("oauth-id", sessionData.oauth_id); //This won't be required most probably. Need to remove it.
-    block("access-token", sessionData.auth_token);
+    block("Authorization", `JWT ${sessionData.jwt}`);
     block("user-id", sessionData.user_id);
   }
 });
