448ff6cec7f4a86ee5a296be45fc598048b5cffb
Prajjwal Singh <sin@prajjwal.com>
2017-08-20 12:45:14 +0530

    Fix: Only show first name in sidebar

---
+++ tests/unit/helpers/first-name-test.js
@@ -0,0 +1,12 @@
+
+import { firstName } from 'hack/helpers/first-name';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | first name');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = firstName([42]);
+  assert.ok(result);
+});
+
