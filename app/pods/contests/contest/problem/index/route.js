a6ed9f13de1d6568cdd3cc4550ae8d9f9de50588
Siddharth Dungarwal <sd5869@gmail.com>
2017-08-30 18:00:21 +0530

    Code Refactoring

--- app/pods/contests/contest/problem/index/route.js
+++ app/pods/contests/contest/problem/index/route.js
@@ -19,7 +19,7 @@ export default Ember.Route.extend({
     let contest = this.modelFor('contests.contest').contest;
     let editorial = null;
     let problem_id = this.modelFor('contests/contest/problem').problem_id;
-    if(contest.isFinished) {
+    if(contest.get('isFinished') == "true") {
       editorial = this.store.queryRecord('editorial' , {p_id:problem_id});
     }
     return Ember.RSVP.hash({
