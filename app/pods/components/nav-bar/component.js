246e73c8c14b7d3e6fa8392d36edb964d8e621a5
Varun Hasija <warunhasija@gmail.com>
2017-11-28 21:56:59 +0530

    Revert "Sso session"

--- app/pods/components/nav-bar/component.js
+++ app/pods/components/nav-bar/component.js
@@ -9,29 +9,32 @@ export default Ember.Component.extend({
     notifications: Ember.A(),
     navBarInitDone: false,
     init () {
-      this._super(...arguments);
-      this.get('store').query('notification', {}).then(result=>{
-          this.set('notifications', result)
-          let readNoti = parseInt(window.localStorage.getItem("readNoti"));
-          if (readNoti===null || readNoti === undefined) {
-              readNoti = 0;
-          }
-          let notiCount = result.get('meta').notiCount - readNoti;
-          if (!notiCount)
-          {
-            notiCount = 0;
-          } else { 
-            this.set('notiCount',result.get('meta').notiCount - readNoti) // - loc storage
-          }
-      })
-      
-      this.set("logoutURL", config.oneauthURL + 'logout?redirect=' + config.publicUrl + '/logout')
+        this.get('store').query('notification', {}).then(result=>{
+            this.set('notifications', result)
+            let readNoti = parseInt(window.localStorage.getItem("readNoti"));
+            if (readNoti===null || readNoti === undefined) {
+                readNoti = 0;
+            }
+            let notiCount = result.get('meta').notiCount - readNoti;
+            if (!notiCount)
+            {
+              notiCount = 0;
+            } else { 
+              this.set('notiCount',result.get('meta').notiCount - readNoti) // - loc storage
+            }
+        })
+        // console.log(this.get('notifications.meta').notiCount);
+       // this.set('notiCount',this.get('notifications').get('meta').notiCount);
+       this._super(...arguments);
    },
    actions: {
-      storeNotiCount() {
-          let notiCount = this.get('notiCount');
-          window.localStorage.setItem("readNoti",notiCount);
-          this.set('notiCount',0);
-      }
+       invalidateSession() {
+            this.get('session').invalidate();
+        },
+        storeNotiCount() {
+            let notiCount = this.get('notiCount');
+            window.localStorage.setItem("readNoti",notiCount);
+            this.set('notiCount',0);
+        }
    }
 });
--- app/pods/logout/route.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-	session: Ember.inject.service(),
-	beforeModel () {
-		this.get('session').invalidate()
-	}
-});
