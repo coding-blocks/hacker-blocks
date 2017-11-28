a12226d1c20a3d4ecf8a7a9898cdf79bc86c73e8
Abhishek Gupta <hi@itsabhishek.me>
2017-11-28 13:56:26 +0530

    redirect to oneauth on logout

--- app/pods/components/nav-bar/component.js
+++ app/pods/components/nav-bar/component.js
@@ -9,32 +9,29 @@ export default Ember.Component.extend({
     notifications: Ember.A(),
     navBarInitDone: false,
     init () {
-        this.get('store').query('notification', {}).then(result=>{
-            this.set('notifications', result)
-            let readNoti = parseInt(window.localStorage.getItem("readNoti"));
-            if (readNoti===null || readNoti === undefined) {
-                readNoti = 0;
-            }
-            let notiCount = result.get('meta').notiCount - readNoti;
-            if (!notiCount)
-            {
-              notiCount = 0;
-            } else { 
-              this.set('notiCount',result.get('meta').notiCount - readNoti) // - loc storage
-            }
-        })
-        // console.log(this.get('notifications.meta').notiCount);
-       // this.set('notiCount',this.get('notifications').get('meta').notiCount);
-       this._super(...arguments);
+      this._super(...arguments);
+      this.get('store').query('notification', {}).then(result=>{
+          this.set('notifications', result)
+          let readNoti = parseInt(window.localStorage.getItem("readNoti"));
+          if (readNoti===null || readNoti === undefined) {
+              readNoti = 0;
+          }
+          let notiCount = result.get('meta').notiCount - readNoti;
+          if (!notiCount)
+          {
+            notiCount = 0;
+          } else { 
+            this.set('notiCount',result.get('meta').notiCount - readNoti) // - loc storage
+          }
+      })
+      
+      this.set("logoutURL", config.oneauthURL + 'logout?redirect=' + config.publicUrl + '/logout')
    },
    actions: {
-       invalidateSession() {
-            this.get('session').invalidate();
-        },
-        storeNotiCount() {
-            let notiCount = this.get('notiCount');
-            window.localStorage.setItem("readNoti",notiCount);
-            this.set('notiCount',0);
-        }
+      storeNotiCount() {
+          let notiCount = this.get('notiCount');
+          window.localStorage.setItem("readNoti",notiCount);
+          this.set('notiCount',0);
+      }
    }
 });
