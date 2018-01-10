33254ab5a3d8fed5abb45cce089eb4ee4f632d0b
hereisnaman <naman@outlook.in>
2018-01-10 18:42:02 +0530

    Added Link to course name on progfile page

--- app/pods/users/user/controller.js
+++ app/pods/users/user/controller.js
@@ -3,18 +3,16 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
   toast: Ember.inject.service(),
-  actions:{
-    submitBatchCode(user)
-    {
+  actions: {
+    submitBatchCode(user) {
       var toast = this.get('toast');
       user.save().then(result => {
-        toast.info('Batch Code Saved!','Message');
+        toast.info('Batch Code Saved!', 'Message');
       })
         .catch(error => {
           Raven.captureException(error);
-          toast.info('Something Went Wrong!','Message');
+          toast.info('Something Went Wrong!', 'Message');
         });
     }
   },
-
 });
