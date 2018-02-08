073b14ae406bdddadd0434c41ecdc8d72725697d
Abhishek Gupta <hi@itsabhishek.me>
2018-02-08 02:53:33 -0700

    undo ugly hotfix

--- app/pods/contests/contest/problem/controller.js
+++ app/pods/contests/contest/problem/controller.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     redirectToContest (model) {
-      //this.transitionToRoute('contests.contest.contest-result', model.contest.id);
+      this.transitionToRoute('contests.contest.contest-result', model.contest.id);
     }
   }
 });
