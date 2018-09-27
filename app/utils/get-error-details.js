653869e7502a8ebb2065f102d324b9fa74304c47
witty123 <warunhasija@gmail.com>
2018-09-27 16:31:09 +0530

    private contest message update

--- app/utils/get-error-details.js
+++ app/utils/get-error-details.js
@@ -1,7 +1,6 @@
 var getErrorDescription = function(code) {
     let desc = {
-     "402":`This contest is for the students enrolled in our courses. 
-            Click <a href="https://codingblocks.com/courses/">here</a> to check out the courses which we have. `,
+     "402":`This is a private contest. Please contact support@codingblocks.com or enroll <a href="https://codingblocks.com">now</a> to get full access to Hackerblocks. Click <a href="https://codingblocks.com/">here</a> to check out the courses which we have. `,
      "405":`Your Email is not verified. 
             Click <a href="https://account.codingblocks.com/users/me">here</a> to verify your email.`,
     };
