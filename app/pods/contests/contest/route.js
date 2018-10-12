923fd6f550ff5eca19430ea411359bb3e79fa621
Prajjwal Singh <sin@prajjwal.com>
2018-10-12 16:36:36 +0530

    Add: Better error handling for inacessible contests

--- app/pods/contests/contest/route.js
+++ app/pods/contests/contest/route.js
@@ -5,16 +5,24 @@ import Env from '../../../config/environment';
 const { inject: { service }, Route } = Ember;
 
 export default Ember.Route.extend({
+  toast: Ember.inject.service (),
   model (params) {
     let contestId = params.contest_id;
     return Ember.RSVP.hash({
       contest: this.get('store').findRecord('contest', contestId, {reload: true})
     })
   },
+
   actions: {
     error (err, transition) {
       if (err instanceof DS.AdapterError) {
-        this.transitionTo('info', 402)
+        transition.abort ()
+
+        this.transitionTo (history.state.path)
+
+        this
+          .get ('toast')
+          .error ('You do not have permission to access that contest!')
       } 
     }
   }
