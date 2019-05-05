1e14e9eccf0b97c4ffa0d75ca96d2949dd37df6d
Jatin Katyal <jatin.katyal13@gmail.com>
2019-05-05 15:29:53 +0530

    compute date on backend

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -2,10 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    let day = moment(moment().unix(), 'X').format("YYYYMMDD");
-
     return Ember.RSVP.hash({
-      dailyCBs: this.get('store').query('dailycb', {count: 4, day}),
+      dailyCBs: this.get('store').query('dailycb', {count: 4}),
       contests: this.get('store').query('contest', {custom :  { ext : 'url',url : 'public'} }),
     });
   },
