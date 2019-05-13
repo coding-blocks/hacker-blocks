5cb553280213df71631ee9cbc3d483bed7b839f2
Jatin Katyal <jatin.katyal13@gmail.com>
2019-05-13 14:09:22 +0530

    dcb fix

--- app/pods/dcb/weekly-problem/route.js
+++ app/pods/dcb/weekly-problem/route.js
@@ -1,17 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  store: Ember.inject.service(),
   breadCrumb: {
     title: 'Problem'
   },
   model(params) {
+    const problemAttempt = this.get('store').createRecord ('problemAttempt', {
+      contestId: 0,
+      problemId: params.problem_id
+    }).save ()
     return Ember.RSVP.hash({
+      problemAttempt,
       p_id: params.problem_id,
       dailycb: this.get('store').queryRecord('dailycb', {custom: {ext: 'url', url: params.problem_id}})
     });
   },
   afterModel(model, transition) {
-  const {dailycb} = model;
-  this.set('breadCrumb.title',dailycb.get('problem.name'));
+    const {dailycb} = model;
+    this.set('breadCrumb.title',dailycb.get('problem.name'));
   }
 });
