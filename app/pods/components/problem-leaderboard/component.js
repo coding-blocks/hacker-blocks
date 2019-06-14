909325b576f79d7699ed83afc0ce6ad6a6ab8665
Jatin Katyal <jatin.katyal13@gmail.com>
2019-06-14 19:38:08 +0530

    fix: dont send all at all in DCB

--- app/pods/components/problem-leaderboard/component.js
+++ app/pods/components/problem-leaderboard/component.js
@@ -25,7 +25,14 @@ export default Ember.Component.extend({
 
     let contestId = this.get('contestId');
     let problemId = this.get('problemId');
-    let customParams = {contest_id: contestId, custom: {ext: 'url', url: 'leaderboard'}, all: this.get('all') || false};
+    let customParams = {
+      contest_id: contestId, 
+      custom: {
+        ext: 'url', 
+        url: 'leaderboard'
+      }, 
+      ...(this.get('all') ? {all: true} : {})
+    };
     if (problemId) {
       customParams.problem_id = problemId;
     }
