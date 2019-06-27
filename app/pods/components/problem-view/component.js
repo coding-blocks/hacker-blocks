6a736e557b1f9e679e311004f1524f48134d79ad
Jatin Katyal <jatin.katyal13@gmail.com>
2019-06-27 18:54:21 +0530

    fix: problem name on user page

--- app/pods/components/problem-view/component.js
+++ app/pods/components/problem-view/component.js
@@ -1,28 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    submissionResult: null,
-    didRender() {
-        this._super(...arguments);
-        let submission = this.get('problem.top');
-/*
-     submissionResult -1 WA
-     submissionResult  0 Partially Solved
-     submissionResult  1 Correct Answer
-*/
-        if (submission.get('isTopSubmission')) {
-           if (submission.get('result') === -1) {
-                this.set('submissionResult', -1);
-            } else if (submission.get('result') === 1) {
-                this.set('submissionResult', 1);
-            } else if (submission.get('result') === 0) {
-                if (submission.get('score') === 0) {
-                    this.set('submissionResult', -1);
-                } else if (submission.get('score') > 0) {
-                    this.set('submissionResult', 0);
-                }
-            }
-        }
-    }
+    submissionResult: null
 
 });
